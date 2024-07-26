d51f59b7bcd22ac07c1b278c7cb28434d01bb7c2
emilLMC <emilgeorgethomas95@gmail.com>
2024-07-26 07:37:47 +0400

    Basic exercie initial commit

---
+++ Basic Exercise.R
@@ -0,0 +1,14 @@
+# R Basics Exercise
+# We will test you on a few topics:
+#   Basic Data Types
+# Basic Arithmetic
+# Vector
+# Vector Operations
+# Comparison operators
+# Vector Selection and Indexing
+# The exercises below should be straightforward if you've reviewed the lecture, don't expect something very challenging yet, this is more of a review to make sure you've understood everything so far! Later on exercises/projects will be more challenging (and a lot more fun!)
+
+# What is two to the power of five?
+2^5
+
+

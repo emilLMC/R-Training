848c3eec94b0e45162e9f499f8b1c2c66f5a57ec
emilLMC <emilgeorgethomas95@gmail.com>
2024-07-30 00:39:30 +0400

    exercise 2completeed

--- RMatrixExercise.R
+++ RMatrixExercise.R
@@ -11,4 +11,12 @@ rbind(A,B)
 c<-c(A,B)
 d<-matrix(c,byrow=T,nrow=2)
 rownames(d)<-c("A","B")
-d
+print(d)
+# EXERCiSE 2
+# Ex 2: Create a 3 by 3 matrix consisting of the numbers 1-9. 
+# Create this matrix using the shortcut 1:9 and 
+# by specifying the nrow argument in the matrix() function call.
+# Assign this matrix to the variable mat
+# JFYI - to delete a variable rm(three)
+mat<-matrix(1:9,nrow=3)
+print(mat)
